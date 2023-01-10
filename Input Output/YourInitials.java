3aad177de1d21388e7bf4c992257ff6f625a9c9a
sarthaksemwal <109014470+sarthaksemwal@users.noreply.github.com>
2023-01-10 10:27:10 +0530

    Create YourInitials.java

---
+++ Input Output/YourInitials.java
@@ -0,0 +1,15 @@
+//creating my initals ss using the printline function //
+
+public class YourInitials {
+    //main function//
+    public static void main(String[] args) {
+   System.out.println(" sssss     sssss ");
+   System.out.println("s     s   s     s");
+   System.out.println("s         s      ");
+   System.out.println(" sssss     sssss");
+   System.out.println("      s         s");
+   System.out.println("s     s   s     s");
+   System.out.println(" sssss     sssss");
+
+    }
+}
