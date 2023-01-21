3bf52220db7fe54fd8a15615dcf751b0a0a57c1f
sarthaksemwal <109014470+sarthaksemwal@users.noreply.github.com>
2023-01-21 10:41:30 +0530

    Create Area.java

---
+++ Functions/Area.java
@@ -0,0 +1,13 @@
+public class Area {
+    public static void main(String[] args) {
+        circle(2, 2);
+        
+    }
+    public static void circle(double length, double width){
+    double c;
+    c=length*width;
+    System.out.println("Area:"+c);
+   
+    }
+    
+}
