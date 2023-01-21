a41be4520237a438e11fa3e38901a1c08813e817
sarthaksemwal <109014470+sarthaksemwal@users.noreply.github.com>
2023-01-21 10:18:32 +0530

    Create Newfunction.java

---
+++ Functions/Newfunction.java
@@ -0,0 +1,18 @@
+class Function{
+    /*A function is a set of code that runs when its called it is used to save time and for the code reuseablity 
+     * public void name (int param ){} here public is level of acess , void is its return type and name is the function name 
+     * here i have made a simple function that sings a song
+     *also remember a function should return a vlaue not print it like below example 
+    */
+    public static void main(String[] args) {
+        SingTumere();
+        
+    }
+    public static void SingTumere(){
+        System.out.println("Ayese kya chale hawa ");
+        System.out.println("Ki le gyi mere sason ko tujse dooor");
+        System.out.println("Mere paasssss");
+        System.out.println("Aur muje hua ahesaaaas ");
+        System.out.println("tu tu tu tu mere ri ri mai tera");
+    }
+}
