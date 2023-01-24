e421c1c699d853b365af7258beddffa8f9f46bdb
sarthaksemwal <109014470+sarthaksemwal@users.noreply.github.com>
2023-01-24 10:56:43 +0530

    Update Area.java

--- Functions/Area.java
+++ Functions/Area.java
@@ -1,8 +1,11 @@
 public class Area {
+    //main methoud
     public static void main(String[] args) {
+        //calling function 
         circle(2, 2);
         
     }
+    //defining the function 
     public static void circle(double length, double width){
     double c;
     c=length*width;
