8c4a514de487b4050e7fb04800a3d4eb836c3968
sarthak <sarthaksemwal>
2023-07-26 15:25:26 +0530

    updated the comment

--- ControlFlow/Count.java
+++ ControlFlow/Count.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class Count {
-    //Note we use for loop when we know how many times we have to run the code;
+    //Note we use for loop when we know how many times we have to run the code
+
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
        System.out.println("Choose a no you want to count through:");
