2b1bceff902d801d3e118ea9df731c3f3a2ef1ea
sarthak <sarthaksemwal69@gmail.com>
2023-12-23 10:20:44 +0530

    added a comment

--- Array/Occurence.java
+++ Array/Occurence.java
@@ -1,4 +1,8 @@
 public class Occurence {
+    /*
+    Given a sorted array Arr of size N and a number X,
+    you need to find the number of occurrences of X in Arr.
+     */
 
       int count(int[] arr, int n, int x) {
         
